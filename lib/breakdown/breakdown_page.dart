5aa037c56f2ec36221a412f5e25163c36bcd1e6f
Annt Phyo Thwin <blacknight.mm@gmail.com>
2022-06-30 11:46:55 +0700

    chore: Added Breakdown page UI

--- lib/breakdown/breakdown_page.dart
+++ lib/breakdown/breakdown_page.dart
@@ -1,12 +1,153 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_banking/statistics/widgets/bar_chart.dart';
 
-class BreakdownPage extends StatelessWidget {
+class BreakdownPage extends StatefulWidget {
   const BreakdownPage({Key? key}) : super(key: key);
 
   @override
+  State<BreakdownPage> createState() => _BreakdownPageState();
+}
+
+class _BreakdownPageState extends State<BreakdownPage> {
+  int touchedIndex = -1;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Breakdown'),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 16),
+          Text(
+            'Breakdown',
+            style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(height: 16),
+          SizedBox(
+            height: 350,
+            child: PieChart(
+              PieChartData(
+                pieTouchData: PieTouchData(
+                    touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                  setState(() {
+                    if (!event.isInterestedForInteractions ||
+                        pieTouchResponse == null ||
+                        pieTouchResponse.touchedSection == null) {
+                      touchedIndex = -1;
+                      return;
+                    }
+                    touchedIndex =
+                        pieTouchResponse.touchedSection!.touchedSectionIndex;
+                  });
+                }),
+                borderData: FlBorderData(show: false),
+                sectionsSpace: 0,
+                centerSpaceRadius: 80,
+                sections: showingSections(),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _showLegend('Bills & Utilities', [Colors.orange.shade300, Colors.orange]),
+              _showLegend('Food & Beverages', [Colors.blue.shade300, Colors.blue]),
+              _showLegend('Travel & Commute', [Colors.pink.shade300, Colors.pink]),
+              _showLegend('Shopping', [Colors.greenAccent.shade100, Colors.greenAccent]),
+              _showLegend('Others', [Colors.deepPurple.shade100, Colors.deepPurpleAccent]),
+            ],
+          )
+        ],
+      ),
     );
+  }
+
+  Widget _showLegend(String name, List<Color> colors) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          dotShape(16, colors),
+          const SizedBox(width: 8),
+          Text(
+            name,
+            style: const TextStyle(
+              fontSize: 18,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<PieChartSectionData> showingSections() {
+    return List.generate(5, (i) {
+      final isTouched = i == touchedIndex;
+      final fontSize = isTouched ? 25.0 : 16.0;
+      final radius = isTouched ? 60.0 : 50.0;
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            color: Colors.blue,
+            value: 20,
+            title: '20%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 1:
+          return PieChartSectionData(
+            color: Colors.orange,
+            value: 30,
+            title: '30%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 2:
+          return PieChartSectionData(
+            color: Colors.deepPurpleAccent,
+            value: 14,
+            title: '14%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 3:
+          return PieChartSectionData(
+            color: Colors.greenAccent,
+            value: 17,
+            title: '17%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 4:
+          return PieChartSectionData(
+            color: Colors.pink,
+            value: 19,
+            title: '19%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        default:
+          throw Error();
+      }
+    });
   }
 }
